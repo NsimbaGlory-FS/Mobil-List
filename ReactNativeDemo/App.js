cc5459d9e7a2651f912061ae97011319574a5032
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-24 22:55:26 -0500

    add

--- ReactNativeDemo/App.js
+++ ReactNativeDemo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Update</Text>
+      <Text>Glory Project</Text>
       <StatusBar style="auto" />
     </View>
   );
