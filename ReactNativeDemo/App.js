6d0f8dd22aa12f14faee97819404aae9948d372e
NsimbaGlory-FS <112911036+NsimbaGlory-FS@users.noreply.github.com>
2024-02-25 00:08:59 -0500

    add

--- ReactNativeDemo/App.js
+++ ReactNativeDemo/App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+import Heading from "./Heading";
 
 export default function App() {
+  //https://crudapi-5b6affb4b9d9.herokuapp.com/api/v1/movies
+  fetch(`https://crudapi-5b6affb4b9d9.herokuapp.com/api/v1/movies`)
+    .then((res) => res.json())
+    .then((data) => console.log({ data }));
+
   return (
-    <View style={styles.container}>
-      <Text>Glory Project</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Mobile Projects</Text>
+      <Heading>This is a Heading</Heading>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
